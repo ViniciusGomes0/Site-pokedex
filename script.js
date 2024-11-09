6a184e3bff98dc66e6cd072b151895013c204fee
ViniciusGomes0 <miriamgisele33@gmail.com>
2024-11-09 00:14:05 -0300

    quarto commit

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-function search() {
-    var query = document.getElementById("search-input").value;
-    if (query) {
-        alert("Você pesquisou por: " + query);
-        // Aqui você pode adicionar lógica para enviar a pesquisa para um servidor ou redirecionar a página
-        // Exemplo de redirecionamento: window.location.href = "https://www.exemplo.com/busca?q=" + query;
-    } else {
-        alert("Por favor, digite algo para pesquisar.");
-    }
+// EFEITO SCROLL
+function scrollToContent(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    const conteudo = document.getElementById('conteudo');
+    conteudo.scrollIntoView({ behavior: 'smooth' }); // Executa a rolagem suave
 }
+
+// EFEITO MUDAR DE ABA
+
+let doctitle = document.title;
+window.addEventListener("blur", () =>{
+    document.title = "Não me abandone (╥_╥)";
+})
+
+window.addEventListener("focus", () =>{
+    document.title = doctitle;
+})
+
+// BUSCAR CARD
+
+const searchInput = document.getElementById('search-input');
+const cards = document.querySelectorAll('.card');
+
+searchInput.addEventListener('input', () => {
+  const searchTerm   
+ = searchInput.value.toLowerCase();
+
+  cards.forEach(card => {
+    const cardTitle = card.querySelector('h3').textContent.toLowerCase();
+
+    if (cardTitle.includes(searchTerm)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
